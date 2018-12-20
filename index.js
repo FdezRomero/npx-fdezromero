bc3e5eb8d8fb987db959adbd0437c12c2b0b72d3
Rodrigo Fernández <rodrigo@savelist.co>
2018-12-20 01:01:05 +0100

    Create personal card

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const { bold, dim, red } = require('colorette');
+
+console.log(`
+  ${bold('Rodrigo Fernández Romero')} / FdezRomero
+  Co-Founder & CTO at ${red('Savelist')} ${dim('(https://savelist.co)')}
+
+  🌍  ${dim('https://')}fdezromero.com
+  🐦  ${dim('https://')}twitter.com/FdezRomero
+  🐙  ${dim('https://')}github.com/FdezRomero
+  👔  ${dim('https://')}linkedin.com/in/rodrigofernandezromero
+
+  📇  npx fdezromero
+`);
